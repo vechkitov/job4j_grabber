bba9490bef2dc344bb893f59f7a0cd6581fb89aa
Sergey <vechkitov@bk.ru>
2022-07-14 21:43:42 +0200

    1. Quartz [#175122]

---
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -0,0 +1,58 @@
+package ru.job4j.quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+public class AlertRabbit {
+    private static final Logger LOG = LoggerFactory.getLogger(AlertRabbit.class);
+    private static Properties cfg = new Properties();
+
+    public static void main(String[] args) {
+        loadProperties();
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(
+                            Integer.parseInt(cfg.getProperty("rabbit.interval"))
+                    )
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException se) {
+            se.printStackTrace();
+        }
+    }
+
+    private static void loadProperties() {
+        String fileName = "rabbit.properties";
+        try (InputStream is = AlertRabbit.class.getClassLoader()
+                .getResourceAsStream(fileName)) {
+            cfg.load(is);
+        } catch (IOException e) {
+            throw new IllegalStateException(
+                    String.format("Не удалось загрузить настройки из файла '%s'", fileName));
+        }
+    }
+
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext context) throws JobExecutionException {
+            LOG.info("Rabbit runs here ...");
+        }
+    }
+}
